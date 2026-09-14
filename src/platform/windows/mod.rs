e85d78633aece4a956958527ddef480595e132ba
Vxiey <84341307+Vxiey@users.noreply.github.com>
2026-09-14 18:57:06 +0200

    style: format Windows exports

--- src/platform/windows/mod.rs
+++ src/platform/windows/mod.rs
@@ -4,8 +4,6 @@ mod input;
 mod precision_clicker;
 
 pub use clock::QpcClock;
-pub use hooks::{
-    CapturedInput, CapturedInputKind, CapturedInputSource, GlobalInputRecorder,
-};
+pub use hooks::{CapturedInput, CapturedInputKind, CapturedInputSource, GlobalInputRecorder};
 pub use input::WindowsInput;
 pub use precision_clicker::{ClickerConfig, LiveClickerConfig, PrecisionClicker};
