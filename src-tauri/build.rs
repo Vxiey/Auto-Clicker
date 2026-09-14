9bd48768b7adb68f60a1084b99548022d79dc3f7
Vxiey <84341307+Vxiey@users.noreply.github.com>
2026-09-14 23:24:17 +0200

    fix: let rustc configure Windows GUI entrypoint

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,11 +1,3 @@
 fn main() {
-    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows")
-        && std::env::var("PROFILE").as_deref() == Ok("release")
-    {
-        // Match Tauri's recommended Windows GUI subsystem for release builds so
-        // launching VxClick does not allocate a console window.
-        println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
-    }
-
     tauri_build::build()
 }
